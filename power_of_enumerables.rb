4a60ba07d39e7ff18ca96862b7a5c6a95e630690
ks2355244 <ks2355244@yahoo.com>
2016-06-07 13:41:38 +0800

    power_of_enumerables edited

--- power_of_enumerables.rb
+++ power_of_enumerables.rb
@@ -1,31 +1,37 @@
 # TODO: Print the elements at indices 1, 3, 5, 7, etc. on separate lines.
 #       You should make use of Enumerable#each_with_index
 def print_odd_indexed_integers(array)
+	array.each do {|array| array % 2 == 0} 
 end
 
 # TODO: Return the odd numbers from a list of integers.
 #       You should make use of Enumerable#select
 def odd_integers(array)
+	array.select do {|array| array % 2 == 1}
 end
 
 # TODO: Return the first number from an Array that is less than a particular number - 'limit.'
 #       You should make use of Enumerable#find
 def first_under(array, limit)
+	array.find {|array| array == (n-1)}
 end
 
 # TODO: Take an Array of Strings and return a new Array with an exclamation point appended to each String.
 #       You should make use of Enumerable#map
 def add_bang(array)
+	array.map == "array.new!"
 end
 
 # TODO: Calculate the sum of an Array of numbers.
 #       You should make use of Enumerable#reduce
 def sum(array)
+	array.reduce{|array| array + "array..n"}
 end
 
 # TODO: Reorganize an Array of the elements into groups of 3, and then sort each group alphabetically.
 #       You should make use of Enumerable#each_slice
 def sorted_triples(array)
+	array.each_slice
 end
 
 # Driver code... don't change this.
